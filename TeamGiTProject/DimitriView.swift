28c6edb8b1be968d61981110d84d9b45a4c40cbe
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-13 11:39:07 +0200

    dimitriView commit

---
+++ TeamGiTProject/DimitriView.swift
@@ -0,0 +1,20 @@
+//
+//  DimitriView.swift
+//  TeamGiTProject
+//
+//  Created by dimitri on 13/05/2022.
+//
+
+import SwiftUI
+
+struct DimitriView: View {
+    var body: some View {
+        Text("Dimitri")
+    }
+}
+
+struct DimitriView_Previews: PreviewProvider {
+    static var previews: some View {
+        DimitriView()
+    }
+}
