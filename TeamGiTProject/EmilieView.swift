bcc6e6db0d6c9ff507f93a609e1a5264d11f4c75
Apprenant 08 <apprenant08@MacBook-Pro-9.local>
2022-05-13 11:36:10 +0200

    emilieVIEX

---
+++ TeamGiTProject/EmilieView.swift
@@ -0,0 +1,20 @@
+//
+//  EmilieView.swift
+//  TeamGiTProject
+//
+//  Created by Apprenant 08 on 13/05/2022.
+//
+
+import SwiftUI
+
+struct EmilieView: View {
+    var body: some View {
+        Text("Emilie")
+    }
+}
+
+struct EmilieView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmilieView()
+    }
+}
