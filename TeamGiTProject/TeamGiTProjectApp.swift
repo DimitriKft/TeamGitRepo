388022b23f7f9229ebfbc4fff165dafc16bd88cc
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-13 10:41:26 +0200

    Initial Commit

---
+++ TeamGiTProject/TeamGiTProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamGiTProjectApp.swift
+//  TeamGiTProject
+//
+//  Created by dimitri on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TeamGiTProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
